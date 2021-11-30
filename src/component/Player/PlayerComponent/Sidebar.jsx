b9f5870a8a8be93a663f636c6d3cdef0d93300a9
reza-habibi <rezhabibi72@gmail.com>
2021-11-30 15:53:14 +0330

    make sidebar better

--- src/component/Player/PlayerComponent/Sidebar.jsx
+++ src/component/Player/PlayerComponent/Sidebar.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import "./Sidebar.style.css";
 import SidebarOption from "./SidebarOption";
-import { FaHome, FaSearch } from "react-icons/fa";
+import { FaHome, FaSearch, FaPlus, FaHeart } from "react-icons/fa";
 import { BiLibrary } from "react-icons/bi";
-
 
 const options = [
   { title: "Home", Icon: FaHome },
@@ -22,6 +21,16 @@ const Sidebar = () => {
       {options.map((option, index) => (
         <SidebarOption key={index} title={option.title} Icon={option.Icon} />
       ))}
+
+      <br />
+      <strong className="sidebar__playlist">
+        <SidebarOption title="Create Playlist" Icon={FaPlus} />
+      </strong>
+      <br />
+      <strong className="sidebar__liked">
+        <SidebarOption title="Liked Song" Icon={FaHeart} />
+      </strong>
+      <hr />
     </div>
   );
 };
